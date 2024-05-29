7b3a1b20e5d58896b9e54e4d1dcafd4fb02d3277
oleoz1n <leopity0@gmail.com>
2024-05-29 15:24:33 -0300

    Update

--- Blocos.sql
+++ Blocos.sql
@@ -42,29 +42,40 @@ BEGIN
     CLOSE PROD_CURSOR;
 END;
 
+
 DECLARE
-    CURSOR prod_cursor is
-        select DISTINCT count(likes.id_post) qtdLikes, post.content_post from T_OP_SR_POST post inner join T_OP_SR_LIKES likes on post.id_post = likes.id_post order by count(likes.id_post) desc;
-    rowp prod_cursor%rowtype;
-    rows_found number := 0;
+    CURSOR prod_cursor IS
+    SELECT DISTINCT
+        COUNT(likes.id_post) AS qtdlikes,
+        post.content_post
+    FROM
+             t_op_sr_post post
+        INNER JOIN t_op_sr_likes likes ON post.id_post = likes.id_post
+    GROUP BY
+        post.id_post,
+        post.content_post
+    ORDER BY
+        qtdlikes DESC;
+
+    rowp       prod_cursor%rowtype;
+    rows_found NUMBER := 0;
 BEGIN
     OPEN prod_cursor();
     dbms_output.put_line('RANK DOS MAIORES NÍVEIS:');
     LOOP
-        FETCH prod_cursor into rowp;
-        exit when prod_cursor%notfound;
+        FETCH prod_cursor INTO rowp;
+        EXIT WHEN prod_cursor%notfound;
         rows_found := rows_found + 1;
-        dbms_output.put_line(rows_found || ' - CONTEUDO: ' || rowp.content_post || ' - XP: ' || rowp.qtdLikes);
-        
-    END LOOP;
-    if rows_found = 0 then
-        dbms_output.put_line('Nenhum usuario com xp maior que 0');
-    end if;
-    CLOSE PROD_CURSOR;
-END;
+        dbms_output.put_line(rows_found
+                             || ' - CONTEUDO: '
+                             || rowp.content_post
+                             || ' - LIKES: '
+                             || rowp.qtdlikes);
 
-SELECT COUNT(DISTINCT likes.id_post) AS qtdLikes, post.content_post
-FROM T_OP_SR_POST post
-INNER JOIN T_OP_SR_LIKES likes ON post.id_post = likes.id_post
-GROUP BY post.id_post, post.content_post
-ORDER BY qtdLikes DESC;
+    END LOOP;
+
+    IF rows_found = 0 THEN
+        dbms_output.put_line('Nenhum usuario com xp maior que 0');
+    END IF;
+    CLOSE prod_cursor;
+END;
